35f30e6b5135c7d06c2c3ba891ab9e2062a56c67
Nagarian47 <onyx_nagarian47@hotmail.fr>
2015-05-18 01:37:58 +0200

    - ajout des images

--- Rapture/app/models/film.rb
+++ Rapture/app/models/film.rb
@@ -2,6 +2,10 @@ class Film < ActiveRecord::Base
   belongs_to :user
 
   def detail
-  	return Tmdb::Movie.detail(self.id_film)
+  	return Tmdb::Movie.detail(id_film)
+  end
+
+  def images
+  	return Tmdb::Movie.images(id_film)
   end
 end
