a7cc6ae39137dd45a80373c9edb7f0fb4978b8b3
Nagarian47 <onyx_nagarian47@hotmail.fr>
2015-05-19 20:19:22 +0200

    - ajout de la navigation complète et fonctionnelle

--- Rapture/app/controllers/application_controller.rb
+++ Rapture/app/controllers/application_controller.rb
@@ -1,16 +1,23 @@
 class ApplicationController < ActionController::Base
+  require 'themoviedb'
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  require 'themoviedb'
 
   before_filter :set_config
-  Tmdb::Api.key("78bf89f89e8823557d092c0602767586")
-  # before_action :authenticate_user!
+  layout :layout
 
-  def set_config
-  	@configuration = Tmdb::Configuration.new
-  end
+  private
+    Tmdb::Api.key("78bf89f89e8823557d092c0602767586")
+    # before_action :authenticate_user!
+    def set_config
+    	@configuration = Tmdb::Configuration.new
+    end
+
+    def layout
+      !devise_controller? && "application"
+    end
 end
 
 
