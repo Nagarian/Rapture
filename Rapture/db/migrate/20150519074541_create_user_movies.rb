2a918fcc850d2d04ac365bcb5d3edee3dfe3ec71
Nagarian47 <onyx_nagarian47@hotmail.fr>
2015-05-19 16:25:01 +0200

    - mise à plat de la BDD

--- Rapture/db/migrate/20150519074541_create_user_movies.rb
+++ Rapture/db/migrate/20150519074541_create_user_movies.rb
@@ -1,7 +1,8 @@
 class CreateUserMovies < ActiveRecord::Migration
   def change
     create_table :user_movies do |t|
-      t.string :movie_id
+      t.integer :movie_id
+      t.integer :user_id
       t.boolean :is_seen
 
       t.timestamps null: false
--- Rapture/db/schema.rb
+++ Rapture/db/schema.rb
@@ -16,18 +16,9 @@ ActiveRecord::Schema.define(version: 20150519074541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "films", force: :cascade do |t|
-    t.integer  "id_film"
-    t.boolean  "seen"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "films", ["user_id"], name: "index_films_on_user_id", using: :btree
-
   create_table "user_movies", force: :cascade do |t|
-    t.string   "movie_id"
+    t.integer  "movie_id"
+    t.integer  "user_id"
     t.boolean  "is_seen"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -51,5 +42,4 @@ ActiveRecord::Schema.define(version: 20150519074541) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "films", "users"
 end
