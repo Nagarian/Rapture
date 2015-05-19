f38129952acdb82e5e25c1ef0ae067ae9a559d63
Nagarian47 <onyx_nagarian47@hotmail.fr>
2015-05-19 09:53:51 +0200

    - modification des méthodes de MoviesController

--- Rapture/app/controllers/movies_controller.rb
+++ Rapture/app/controllers/movies_controller.rb
@@ -1,12 +1,19 @@
 class MoviesController < ApplicationController
 
   # GET /movies.json
-  def home
-    @movies = Tmdb::Movie.upcoming
+  def popular
+    @movies = Tmdb::Movie.popular
     respond_to do |format|
       format.html { render "movies/home", layout: false }
       format.json { render json: @movies }
     end
   end
 
+  def upcoming
+    @movies = Tmdb::Movie.upcoming
+    respond_to do |format|
+      format.html { render "movies/home", layout: false }
+      format.json { render json: @movies }
+    end
+  end
 end
--- Rapture/config/routes.rb
+++ Rapture/config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  get 'movies/home'
+  get 'movies/popular'
+  get 'movies/upcoming'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
